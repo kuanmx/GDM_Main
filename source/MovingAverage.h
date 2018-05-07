2deae634ef4a01c0609a1a33b3964e26209db21c
Poh Zhi-Ee <poh140996@hotmail.com>
2018-05-07 21:21:22 +0800

    optimised MovingAverage

--- source/MovingAverage.h
+++ source/MovingAverage.h
@@ -15,10 +15,9 @@
 template<typename T, int N>
 class MovingAverage {
 public:
-    MovingAverage()
-    {
-        static_assert(N>0, "\nNumber of elements has to be greater than 0\n");
-        static_assert(N%2==1, "\nNumber of elements has to be odd (for stability)\n");
+    MovingAverage(){
+        static_assert(N>0, "Number of elements has to be greater than 0");
+        static_assert(N%2==1, "Number of elements has to be odd (for stability)");
     }
     explicit MovingAverage(T initialData);
     void AddData(T data);
@@ -33,11 +32,12 @@ public:
 private:
     std::deque<T> _dataList;
     T _value;
+    const double N_recipro = 1.0/N;
 };
 
 
 template<typename T, int N>
-MovingAverage<T, N>::MovingAverage(T initialData) : _value(initialData)
+MovingAverage<T, N>::MovingAverage(T initialData) :  _value(initialData)
 {
     MovingAverage();
     _dataList.push_back(initialData);
@@ -46,18 +46,23 @@ MovingAverage<T, N>::MovingAverage(T initialData) : _value(initialData)
 template<typename T, int N>
 void MovingAverage<T, N>::AddData(T data)
 {
+    T sum = 0;
     if(_dataList.size()<N){
         _dataList.push_back(data);
+        for(auto &val: _dataList){
+            sum += val;
+        }
+        _value = sum/_dataList.size();
     }
     else{
         _dataList.pop_front();
         _dataList.push_back(data);
+        for(auto &val: _dataList){
+            sum += val;
+        }
+        _value = sum*N_recipro;
     }
-    T sum = 0;
-    for(auto &val: _dataList){
-        sum += val;
-    }
-    _value = sum/_dataList.size();
 }
+
 
 #endif //MOVINGAVERAGE_H
